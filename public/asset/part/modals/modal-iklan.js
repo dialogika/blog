d70a1686e03e6232d5a57d4aeccebda16b52b2b3
Yoga723 <yogapangestu16@gmail.com>
2025-06-29 12:49:05 +0700

    tambah file index

---
+++ public/asset/part/modals/modal-iklan.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const adModal = new bootstrap.Modal(document.getElementById("adModal"));
+  const adStateKey = "hasSeenAd";
+  const adTimestampKey = "adSeenTimestamp";
+  const now = new Date().getTime();
+  const seenTimestamp = localStorage.getItem(adTimestampKey);
+
+  if (!seenTimestamp || now - seenTimestamp > 10 * 60 * 1000) {
+    adModal.show();
+    // 10 minutes
+    localStorage.setItem(adStateKey, "true");
+    localStorage.setItem(adTimestampKey, now);
+  }
+});
