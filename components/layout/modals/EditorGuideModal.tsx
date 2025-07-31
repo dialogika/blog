91a8d7c10212359e60b646e14e7583688b5b6f93
Yoga723 <yogapangestu16@gmail.com>
2025-07-31 23:46:27 +0700

    tambah video tutor di guide

--- components/layout/modals/EditorGuideModal.tsx
+++ components/layout/modals/EditorGuideModal.tsx
@@ -32,8 +32,7 @@ const EditorGuideModal = () => {
           position: "fixed",
           bottom: 20,
           right: 20,
-        }}
-      >
+        }}>
         Buka Guide
       </button>
       <Modal
@@ -42,48 +41,71 @@ const EditorGuideModal = () => {
         onHide={() => setShowGuide(false)}
         className="modal-xl"
         dialogClassName="modal-90w" // custom class
-        aria-labelledby="guide-modal"
-      >
-        <Modal.Header closeButton className="fs-4 fw-bold">
+        aria-labelledby="guide-modal">
+        <Modal.Header
+          closeButton
+          className="fs-4 fw-bold">
           Guide & Tips untuk editor
         </Modal.Header>
         <Modal.Body>
           <Accordion>
-            {/* Start of Guide memasukkan Gambar */}
             <Accordion.Item eventKey="0">
-              <Accordion.Header>
-                Cara Memasukkan Gambar (Gambar yang Dipilih dari Internet)
-              </Accordion.Header>
+              <Accordion.Header>Tutorial menggunakan tools membuat artikel Dialogika</Accordion.Header>
+              <Accordion.Body className="">
+                <iframe
+                  width="560"
+                  height="315"
+                  src="https://www.youtube.com/embed/tylqNTRt9tE?si=r9_YgLekqZt-9rtp"
+                  title="YouTube video player"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerPolicy="strict-origin-when-cross-origin"
+                  allowFullScreen></iframe>
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>Tutorial Mengedit Artikel yang sudah publish</Accordion.Header>
+              <Accordion.Body className="">
+                <iframe
+                  width="560"
+                  height="315"
+                  src="https://www.youtube.com/embed/j3TjUCO9sBU?si=cVPK1YoIdgu4FrTm"
+                  title="YouTube video player"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerPolicy="strict-origin-when-cross-origin"
+                  allowFullScreen></iframe>
+              </Accordion.Body>
+            </Accordion.Item>
+            {/* Start of Guide memasukkan Gambar */}
+            <Accordion.Item eventKey="2">
+              <Accordion.Header>Cara Memasukkan Gambar (Gambar yang Dipilih dari Internet)</Accordion.Header>
               <Accordion.Body className="">
                 <h5>
                   1. Cari gambar di internet. Contoh :{" "}
                   <a
                     href="https://www.pexels.com/"
                     target="_blank"
-                    className="text-decoration-underline "
-                  >
+                    className="text-decoration-underline ">
                     Pexels
                   </a>
                   ,{" "}
                   <a
                     href="https://yandex.com/images/"
                     target="_blank"
-                    className="text-decoration-underline "
-                  >
+                    className="text-decoration-underline ">
                     Yandex
                   </a>
                   ,{" "}
                   <a
                     href="https://unsplash.com/"
                     target="_blank"
-                    className="text-decoration-underline "
-                  >
+                    className="text-decoration-underline ">
                     Unsplash
                   </a>
                 </h5>
                 <h5 className="mt-5">
-                  2. Klik kanan pada gambar dan pilih{" "}
-                  <b>`Open Image in New Tab`</b>
+                  2. Klik kanan pada gambar dan pilih <b>`Open Image in New Tab`</b>
                 </h5>
                 <Image
                   src={guideGambar1}
@@ -92,9 +114,8 @@ const EditorGuideModal = () => {
                   alt="guide gambar 1 "
                 />
                 <h5 className="mt-5">
-                  3. Copy link gambarnya. Kembali ke halaman text editor dan di
-                  toolbar klik icon gambar (dibagian kanan atas) {`->`} masukkan
-                  linknya ke URL dan submit
+                  3. Copy link gambarnya. Kembali ke halaman text editor dan di toolbar klik icon gambar (dibagian kanan
+                  atas) {`->`} masukkan linknya ke URL dan submit
                 </h5>
                 <Image
                   src={guideGambar2}
@@ -104,9 +125,8 @@ const EditorGuideModal = () => {
                 />
 
                 <h5 className="mt-5">
-                  4. Klik gambarnya untuk kustomisasi, gambar bisa diperbesar
-                  atau diperkecil. Pilih Edit {`->`} Advanced dan masukkan
-                  `dialogika-img` di Classes dan Id kemudian <i>Apply</i>.
+                  4. Klik gambarnya untuk kustomisasi, gambar bisa diperbesar atau diperkecil. Pilih Edit {`->`}{" "}
+                  Advanced dan masukkan `dialogika-img` di Classes dan Id kemudian <i>Apply</i>.
                 </h5>
                 <Image
                   src={guideGambar3}
@@ -116,8 +136,7 @@ const EditorGuideModal = () => {
                 />
 
                 <h5 className="mt-5">
-                  5. Bila ingin ada text disamping gambar, pilih vertical align
-                  dan kemudian Klik disamping gambar
+                  5. Bila ingin ada text disamping gambar, pilih vertical align dan kemudian Klik disamping gambar
                 </h5>
                 <Image
                   src={guideGambar4}
@@ -130,81 +149,57 @@ const EditorGuideModal = () => {
             {/* End of Guide memasukkan Gambar */}
 
             {/* Start of Guide 2 */}
-            <Accordion.Item eventKey="1">
-              <Accordion.Header>
-                Cara Memasukan Gambar Dari Text Editor Langsung
-              </Accordion.Header>
+            <Accordion.Item eventKey="3">
+              <Accordion.Header>Cara Memasukan Gambar Dari Text Editor Langsung</Accordion.Header>
               <Accordion.Body>
-                <h5>
-                  1. Pilih Disebalah pojok kanan bawah text editor, untuk
-                  mencari gambar dari text editor
-                </h5>
+                <h5>1. Pilih Disebalah pojok kanan bawah text editor, untuk mencari gambar dari text editor</h5>
                 <Image
                   src={guideGambar5}
                   className="img-fluid w-auto mx-auto rounded-3"
                   style={{ height: "auto" }}
                   alt="guide gambar 5 "
                 />
-                <h5 className="mt-5">
-                  2. Masukan Qword di kotak pencarian dan klik Cari Gambar.
-                </h5>
+                <h5 className="mt-5">2. Masukan Qword di kotak pencarian dan klik Cari Gambar.</h5>
                 <Image
                   src={guideGambar6}
                   className="img-fluid w-auto mx-auto rounded-3"
                   style={{ height: "auto" }}
                   alt="guide gambar 6 "
                 />
-                <h5 className="mt-5">
-                  3. Silahkan pilih gambar yang diinginkan
-                </h5>
-                <h5 className="mt-5">
-                  4. Jika merasa gambar yang diinginkan bisa pindah page, dengan
-                  mengeklik
-                </h5>
+                <h5 className="mt-5">3. Silahkan pilih gambar yang diinginkan</h5>
+                <h5 className="mt-5">4. Jika merasa gambar yang diinginkan bisa pindah page, dengan mengeklik</h5>
                 <Image
                   src={guideGambar7}
                   className="img-fluid w-auto mx-auto rounded-3"
                   style={{ height: "auto" }}
                   alt="guide gambar 7 "
                 />
-                <h5 className="mt-5">
-                  5. Kilk gambar yang diinginkan dan gambar akan muncul di text
-                  editor
-                </h5>
+                <h5 className="mt-5">5. Kilk gambar yang diinginkan dan gambar akan muncul di text editor</h5>
               </Accordion.Body>
             </Accordion.Item>
             {/* End of Guide 2 */}
 
             {/* Start of Guide 3 */}
-            <Accordion.Item eventKey="2">
+            <Accordion.Item eventKey="4">
               <Accordion.Header>Cara Membuat Inbound Link</Accordion.Header>
               <Accordion.Body>
                 {" "}
-                <h5>
-                  1. Pilih disebelah pojok kanan, untuk menambahkan inbound link
-                </h5>
+                <h5>1. Pilih disebelah pojok kanan, untuk menambahkan inbound link</h5>
                 <Image
                   src={guideGambar8}
                   className="img-fluid w-auto mx-auto rounded-3"
                   style={{ height: "auto" }}
                   alt="guide gambar 7 "
                 />
-                <h5 className="mt-5">
-                  2. Masukan URL yang akan menjadi Inbound Link
-                </h5>
-                <h5 className="mt-5">
-                  3. Setelah Inbound Link terbuat, pilih logo pennsil
-                </h5>
+                <h5 className="mt-5">2. Masukan URL yang akan menjadi Inbound Link</h5>
+                <h5 className="mt-5">3. Setelah Inbound Link terbuat, pilih logo pennsil</h5>
                 <Image
                   src={guideGambar12}
                   className="img-fluid w-auto mx-auto rounded-3"
                   style={{ height: "auto" }}
                   alt="guide gambar 7 "
                 />
-                <h5 className="mt-5">
-                  4. Checklist pada bagian (Open in new tab) dan ganti text
-                  (Baca Juga: ......)
-                </h5>
+                <h5 className="mt-5">4. Checklist pada bagian (Open in new tab) dan ganti text (Baca Juga: ......)</h5>
                 <Image
                   src={guideGambar9}
                   className="img-fluid w-auto mx-auto rounded-3"
@@ -216,12 +211,10 @@ const EditorGuideModal = () => {
             </Accordion.Item>
             {/* End of Guide 3 */}
 
-            <Accordion.Item eventKey="3">
+            <Accordion.Item eventKey="5">
               <Accordion.Header>Cara Mereset Artikel</Accordion.Header>
               <Accordion.Body>
-                <h5 className="mt-5">
-                  1. Pilih pada bagian X di pojok kanan bawah
-                </h5>
+                <h5 className="mt-5">1. Pilih pada bagian X di pojok kanan bawah</h5>
                 <Image
                   src={guideGambar11}
                   className="img-fluid w-auto mx-auto rounded-3"
@@ -229,8 +222,7 @@ const EditorGuideModal = () => {
                   alt="guide gambar 7 "
                 />
                 <h5 className="mt-5">
-                  2. Akan muncul popup konfirmasi untuk mereset artikel, klik
-                  reset jika ingin meresetnya
+                  2. Akan muncul popup konfirmasi untuk mereset artikel, klik reset jika ingin meresetnya
                 </h5>
                 <Image
                   src={guideGambar10}
@@ -238,9 +230,7 @@ const EditorGuideModal = () => {
                   style={{ height: "auto" }}
                   alt="guide gambar 7 "
                 />
-                <h5 className="mt-5">
-                  3. Artikel akan terreset, dan akan kembali seperti awal
-                </h5>
+                <h5 className="mt-5">3. Artikel akan terreset, dan akan kembali seperti awal</h5>
               </Accordion.Body>
             </Accordion.Item>
           </Accordion>
